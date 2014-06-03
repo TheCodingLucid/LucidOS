b2e0c21546e6b24659baf158302e5dd09870b7cd
TheCodingLucid <ilucidhd@gmail.com>
2014-06-03 08:02:38 -0400

    Update LucidOS.ino

--- LucidOS.ino
+++ LucidOS.ino
@@ -1,16 +1,15 @@
 /*
 Lucid OS - Developed by Blake Sekelsky
-Frequent updates being made on Twitter @LucidOS_Project and on GitHub and Youtube.
-GitHub - https://github.com/LucidOSProject/LucidOS_Project/blob/master/LucidOS.ino
-Youtube - http://www.youtube.com/playlist?list=PLwBbXoTme4Hra0Lc5wSkfXOc5po9NYJ1R
+
+//Nightly Builds At:
+GitHub - https://github.com/TheCodingLucid/LucidOS
+//Video Updates At:
+Youtube - http://www.youtube.com/user/LucidSoftwareStudios
+//Blog Posts At:
 Twitter - https://twitter.com/LucidOS_Project
 */ 
 
 #include <LucidLogo.h>
-#include <font4x6.h>
-#include <font6x8.h>
-#include <font8x8.h>
-#include <font8x8ext.h>
 #include <fontALL.h>
 #include <TVout.h>
 #include <avr/pgmspace.h>
@@ -20,7 +19,8 @@ Twitter - https://twitter.com/LucidOS_Project
   void setup();
   void loop();
   void program();
-   
+  
+  //Setup TVout to display at (126, 96) 
   void setup()  
   {
     TV.begin(NTSC, 126, 96);
